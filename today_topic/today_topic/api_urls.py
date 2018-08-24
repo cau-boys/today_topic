d3b3b8167a271fc3191ffc48d5b272bb3eabf5b6
Unknown <cupjoo@naver.com>
2018-08-25 02:04:06 +0900

    bot/app_name 수정

--- today_topic/today_topic/api_urls.py
+++ today_topic/today_topic/api_urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from today_topic import api_views
 
 
-app_name = 'today_topic'
+app_name = 'api'
 urlpatterns = [
     path('keyboard/', api_views.keyboard, name='keyboard'),
     path('messages/', api_views.messages, name='messages'),
