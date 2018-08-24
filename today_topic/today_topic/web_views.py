b6c538986ec727d936b561b46cb7f95e15ba1125
Unknown <cupjoo@naver.com>
2018-08-25 02:01:57 +0900

    web/get_context_data 함수 override
    get_topics 함수 호출

--- today_topic/today_topic/web_views.py
+++ today_topic/today_topic/web_views.py
@@ -4,3 +4,9 @@ from today_topic.utils import get_topics
 
 class IndexView(TemplateView):
     template_name = 'today_topic/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        category = kwargs.get('category', 'all')
+        context['topics'] = get_topics(5, category)
+        return context
