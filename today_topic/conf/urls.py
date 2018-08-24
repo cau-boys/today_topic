ec2deab83b8c052300f508864de143b5b67e93f9
Unknown <cupjoo@naver.com>
2018-08-24 20:51:26 +0900

    URL 이름 수정

--- today_topic/conf/urls.py
+++ today_topic/conf/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('today_topic_api/', include('today_topic.api_urls', namespace='today_topic_api')),
-    path('today_topic_site/', include('today_topic.web_urls', namespace='today_topic_site')),
+    path('api/', include('today_topic.api_urls', namespace='api')),
+    path('site/', include('today_topic.web_urls', namespace='site')),
 ]
