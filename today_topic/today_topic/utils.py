b4b502a122fbaafbbdc741a1c11363ef6dc10886
Unknown <cupjoo@naver.com>
2018-08-24 20:33:07 +0900

    api 통신 함수 구현

---
+++ today_topic/today_topic/utils.py
@@ -0,0 +1,26 @@
+import json, requests
+
+
+def get_topics(count, category):
+    # json request
+    url = 'http://api.datamixi.com/datamixiApi/topictoday'
+    params = {
+        'key': '3082028134077943630',
+        'count': count,
+        'category': category
+    }
+    res = requests.get(url, params=params)
+
+    # convert json to dictionary
+    res_data = json.loads(res.text)
+    docus = res_data['document']
+
+    # get topic list
+    topics = list()
+    for docu in docus:
+        topic = {}
+        topic['title'] = docu['title']
+        topic['content'] = docu['content']
+        topics.append(topic)
+
+    return topics
