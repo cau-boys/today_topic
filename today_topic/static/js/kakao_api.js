0c1b1034791a8affa26ec198d739295aeeb13be4
Unknown <cupjoo@naver.com>
2018-08-28 12:49:59 +0900

    web/카카오 API 호출
    Modal 삭제

---
+++ today_topic/static/js/kakao_api.js
@@ -0,0 +1,7 @@
+Kakao.init('a1c1a38168d18d6b59734c1a9353b401');
+
+function addPlusFriend() {
+    Kakao.PlusFriend.addFriend({
+        plusFriendId: '_EFwqj' // 플러스친구 홈 URL에 명시된 id로 설정합니다.
+    });
+}
