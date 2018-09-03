4d142550d467efd2f947990f5b8252908252e4a1
Unknown <cupjoo@naver.com>
2018-09-03 21:29:34 +0900

    web/사용자 질문 답변 함수 추가

--- today_topic/today_topic/utils.py
+++ today_topic/today_topic/utils.py
@@ -46,20 +46,48 @@ def trim_topics(topics):
 
     return response_data
 
+
+# convert long url to short url
 def get_short_url(long_url):
     # json request
-    url = 'http://surl.kr/Api/create.php' 
+    url = 'http://surl.kr/Api/create.php'
     params = {
         'type': 'json',
         'longUrl': long_url
     }
     res = requests.get(url, params=params)
-    
-     # convert json to dictionary
+
+    # convert json to dictionary
     res_data = json.loads(res.text)
     if res_data['status'] == 'success':
         short_url = res_data['shortUrl']
     else:
         short_url = res_data['longUrl']
-    print(res_data) 
+    print(res_data)
     return short_url
+
+
+# get an answer of user's question
+def get_answer(question):
+    accuracy = 0.9  # 90 % accuracy
+    category_list = ['entertainment', 'politics', 'economics',
+                     'society', 'it', 'world']
+
+    # request topic_list for all categories
+    for category in category_list:
+        topics = get_topics(30, category)
+
+        # find an answer in topics
+        for topic in topics:
+            url = 'http://api.datamixi.com/datamixiApi/mrcQa'
+            params = {
+                'key': '3082028134077943630',
+                'paragraph': topic['content'],
+                'question': question
+            }
+            res = requests.get(url, params=params)
+
+            # convert json to dictionary
+            result = json.loads(res.text)
+            if result['score'] > accuracy:
+                return result['answer']
