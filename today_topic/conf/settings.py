3c337b0817e8fc5cb836d4c58888e6d41c898c70
Unknown <cupjoo@naver.com>
2018-09-03 21:42:31 +0900

    web/디버그 모드 수정

--- today_topic/conf/settings.py
+++ today_topic/conf/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7v5g9((r)xg1_zelyvl$ydnfeu8+skk6@nekc2jls)bo0=k#d('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
