042cb61d916d791ae902b8a498537b4e6ed8ed41
Junyoung_Choi <cupjoo@naver.com>
2018-10-02 02:08:42 +0900

    불필요한 정적 경로 삭제

--- today_topic/conf/urls.py
+++ today_topic/conf/urls.py
@@ -9,6 +9,3 @@ urlpatterns = [
     path('api/', include('today_topic.api_urls', namespace='api')),
     path('site/', include('today_topic.web_urls', namespace='site')),
 ]
-
-# 정적 파일 Root 경로 등록
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
